df03a77b5f3d162cc6c84d0ab2971ed1c5f363e6
m-cheicki <mcheicki.dev@gmail.com>
2022-01-01 18:38:58 +0100

    🚧 💄 WIP : Text-to-speech screen

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cards } from '../interfaces/card';
 
 @Component({
   selector: 'app-read',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadComponent implements OnInit {
 
+  cards: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.cards = Cards;
   }
 
 }
