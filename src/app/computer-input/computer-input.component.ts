2ab96b2341638f92fcec2278f9183a1886d75efa
m-cheicki <mcheicki.dev@gmail.com>
2022-01-17 16:57:40 +0100

    ✨ Auto translate audio from transcript

--- src/app/computer-input/computer-input.component.ts
+++ src/app/computer-input/computer-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SttService } from '../services/stt.service';
+import { TranslateService } from '../services/translate.service';
 
 @Component({
   selector: 'app-computer-input',
@@ -10,33 +11,24 @@ export class ComputerInputComponent {
 
   @ViewChild('inputAudio') public audioInput!: HTMLInputElement;
 
-  public name !: string;
-  public transcription !: string; 
+  public name: string = "Choisir un fichier";
+  public transcription: string = '';
+  public translation: string = '';
+
 
   public constructor(
+    private _translateService: TranslateService,
     private _sttService: SttService,
   ) { }
 
-  ngOnInit(): void {
-    this.name = "Choisir un fichier";
-  }
+  ngOnInit(): void {  }
 
-  public getName(event: Event) {
+  public initFile(event: Event) {
     const target = event.target as HTMLInputElement;
     const files = target.files as FileList;
-    this.name = files[0].name; 
-  }
-
-  public callApiSTT(files: FileList | null): void {
-    if (!files || files.length === 0)
-      return
-    const file = files[0];
-    this._sttService.transcribeWithProvider(file, 'ibm')
-      .then(result => {
-        this.audioInput.files = null;
-        console.log(result);
-      })
-      .catch(err => console.error(err));
+    this.name = files[0].name;
+    this.transcription = '';
+    this.translation = '';
   }
 
   public callApiSTTAll(files: FileList | null): void {
@@ -52,9 +44,26 @@ export class ComputerInputComponent {
           && (result[1].provider == 'rev' || result[1].provider == 'assembly')) {
           this.transcription = result[0].result;
         }
+
         else {
           this.transcription = result[1].result;
         }
+
+        this.callApiTranslation(this.transcription);
+
+      })
+      .catch(err => console.error(err));
+  }
+
+  public callApiTranslation(text: string | null): void {
+    this.translation = '';
+
+    if (!text)
+      return
+
+    this._translateService.translate(text, 'en', 'google')
+      .then(result => {
+        this.translation = result.translation
       })
       .catch(err => console.error(err));
   }
